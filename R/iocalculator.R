fbc3198bb9e336d21a929e5acabcc6e4e9ef09ae
AlexRichardson-NOAA <alex.richardson@noaa.gov>
2021-04-09 14:15:43 -0400

    Fixing the category/category2 problem

--- R/iocalculator.R
+++ R/iocalculator.R
@@ -33,14 +33,9 @@ io_classifier <- function(data, species = Comm.Catch.Spp.List, year = NA, recall
 
   }
 
-  if (sum(tsn_id$category %in% c("Other", "Uncategorized"))>0) {
-    tsn_id<-tsn_id[!(tsn_id$category %in% c("Other", "Uncategorized")),
-                   c("TSN", "category")]
-  }
-
-  if (sum(tsn_id$category2 %in% c("Other", "Uncategorized"))>0) {
-    tsn_id<-tsn_id[!(tsn_id$category2 %in% c("Other", "Uncategorized")),
-                   c("TSN", "category2")]
+  if (sum(tsn_id$category %in% c("Other", "Uncategorized"))>0 | sum(tsn_id$category2 %in% c("Other", "Uncategorized"))>0) {
+    tsn_id<-tsn_id[!(tsn_id$category %in% c("Other", "Uncategorized")), !(tsn_id$category2 %in% c("Other", "Uncategorized")),
+                   c("TSN", "category", "category2")]
   }
 
   tsn_id$TSN<-as.numeric(as.character(tsn_id$TSN))
